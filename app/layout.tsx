0f60cc147b0cb3ce887feb4fbb922e8da074ccb1
johns-children <taotan421@gmail.com>
2025-02-01 23:59:28 +0900

    fix: Change the home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
       >
         <div>
           <nav className="flex justify-start space-x-4 px-3 py-2 border-b border-grey-500">
+            <Link href="/" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+              Home
+            </Link>
             <Link href="/test" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
               Test
             </Link>
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>This is the test page!</h1>
       <Link href="/">Home Pageへ戻る</Link>
     </div>
   );
